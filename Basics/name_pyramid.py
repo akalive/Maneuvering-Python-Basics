f910b2deea9166486017f336af409d95ebddd0c7
akalive <aparnakharelive@gmail.com>
2016-10-20 17:23:49 +0530

    Update name_pyramid.py

--- Basics/name_pyramid.py
+++ Basics/name_pyramid.py
@@ -1,13 +1,14 @@
 # This Prints a pyramid of 10 rows using a given string
 # Start  by taking user input
 name = input("Please Enter a Name: ")
-name*1
-name*2
-name*3
-name*4
-name*5
-name*6
-name*7
-name*8
-name*9
-name*10
+print(format(name*1).center(80))
+print(format(name*2).center(80))
+print(format(name*3).center(80))
+print(format(name*4).center(80))
+print(format(name*5).center(80))
+print(format(name*6).center(80))
+print(format(name*7).center(80))
+print(format(name*8).center(80))
+print(format(name*9).center(80))
+print(format(name*10).center(80))
+
